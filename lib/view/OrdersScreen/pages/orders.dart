82f1e20397fc46a213ff48dcae2ed2236a54b214
Chiida <faruk.cidic@gmail.com>
2020-09-05 23:10:09 +0200

    reading order from specific user ->manualy, and showing on OrdersScreen

--- lib/view/OrdersScreen/pages/orders.dart
+++ lib/view/OrdersScreen/pages/orders.dart
@@ -115,16 +115,16 @@ class _OrdersPageState extends State<OrdersPage> {
 }
 
 Future getOrders() async {
-  final QuerySnapshot favorites = await Firestore.instance
+  final QuerySnapshot orders = await Firestore.instance
       .collection('users')
       .document('J5Ni9qXk4mN3495LJdIE1EJhJgJ2')
       .collection('orders')
       .document('order1')
       .collection('product1')
       .getDocuments();
-  List<DocumentSnapshot> favoritesDocs = favorites.documents;
+  List<DocumentSnapshot> favoritesDocs = orders.documents;
   orderList.clear();
-  orderList = favorites.documents;
+  orderList = orders.documents;
   print(orderList[0]);
   return favoritesDocs;
 }
